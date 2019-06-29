15305d21856691995bdee4f22bbda6ee628bce4a
Rafael Fernández <rafaelfernandezortiz@gmail.com>
2019-06-29 20:11:01 +0200

    fc

---
+++ g30Loc.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import requests
+import subprocess as sbp
+import shlex as sh
+import yaml
+
+path ="./g30loc.yml"
+cfg = yaml.safe_load(open(path))
+
+def macAdd():
+    sbp.call("iwlist {} scan > .macAddress.txt".format(cfg['tarjeta-red']), shell=True)
+    fich = open(".macAddress.txt", "r")
+    lines = fich.readlines()
+    fich.close()
+    sbp.call('rm .macAddress.txt', shell=True)
+    macAddress = [x[x.find(":")+2:].strip() for x in lines  if 'Address' in x ]
+    signalStrength = [int(x[x.find("vel=")+4:].strip().split()[0]) for x in lines if 'Signal level' in x ]
+    dataMAC = [{"macAddress": macAddress[i], "signalStrength":signalStrength[i],"signalToNoiseRatio":0} for i in range(len(macAddress))]
+    return dataMAC
+
+def main():
+    datos = {
+        "considerIP":"false",
+        "wifiAccessPoints": macAdd()
+    }
+    url = cfg['url']+cfg['key']
+    response = requests.post(url, json=datos)
+    x, y, z = (response.json()['location']['lat']), (response.json()['location']['lng']), (response.json()['accuracy'])
+    print("Enlace\n===========")
+    print(cfg['ubicacion'].format(a=x,b=y))
+    print("\nCoordenadas\n===========")
+    print("Latitud: {}\nLongitud: {}\nRadio: {}m".format(x,y,z))
+
+if __name__ == "__main__":
+    main()
